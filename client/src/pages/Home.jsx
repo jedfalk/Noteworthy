bfad6692381df1f2e6e6ac063a5dd5dcfbfb86b3
Kinnedy <taylorkinnedy@gmail.com>
2025-05-29 18:39:13 -0500

    added navbar too homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import navbar from '../components/Navbar';
+import login from '../components/Login';
+import signup from '../components/Signup';
 
 export default function Home() {
   return (
+    <div>
+      <navbar />
     <div className="text-center">
       <h1>Welcome to NoteSphere</h1>
-      <p className="lead">Your place to take and manage notes securely.</p>
+      <p>Your personal space for notes, ideas, and inspiration.</p>
+    </div>
     </div>
   );
 }
