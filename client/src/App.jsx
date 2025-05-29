6ead4668a2a455617a86b55fe2abfdc4ba1442f9
JedFalk <jedfalk@gmail.com>
2025-05-28 20:33:30 -0500

    added navbar and updated pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
@@ -9,14 +10,17 @@ import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div className="container py-4">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/notes" element={<Notes />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+    <div>
+      <Navbar />
+      <div className="container py-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/notes" element={<Notes />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
     </div>
   );
 }
