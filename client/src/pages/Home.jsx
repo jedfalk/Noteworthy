79ae3c7d48b55379b7cda5798e1afb29f4c5b68a
Kinnedy <taylorkinnedy@gmail.com>
2025-05-29 21:50:25 -0500

    fixed bug on home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
 
 
 export default function Home() {
   return (
-    <div>
-      <Navbar />
     <div className="text-center">
       <h1>Welcome to NoteSphere</h1>
       <p>Your personal space for notes, ideas, and inspiration.</p>
-    </div>
     </div>
   );
 }
