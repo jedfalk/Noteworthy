2e646ea0b3016ffb980cf8667469cbb0fe083003
JedFalk <jedfalk@gmail.com>
2025-06-02 19:42:28 -0500

    added screen sizing

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => setIsOpen(!isOpen);
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
         <Link className="navbar-brand" to="/">Noteworthy</Link>
-        <div>
-          <Link className="btn btn-outline-light me-2" to="/notes">My Notes</Link>
-          <Link className="btn btn-outline-light me-2" to="/login">Login</Link>
-          <Link className="btn btn-outline-light" to="/signup">Sign Up</Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={toggleNavbar}
+          aria-controls="navbarSupportedContent"
+          aria-expanded={isOpen}
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`} id="navbarSupportedContent">
+          <div className="navbar-nav ms-auto">
+            <Link className="btn btn-outline-light me-2 my-1" to="/notes">My Notes</Link>
+            <Link className="btn btn-outline-light me-2 my-1" to="/login">Login</Link>
+            <Link className="btn btn-outline-light my-1" to="/signup">Sign Up</Link>
+          </div>
         </div>
       </div>
     </nav>
--- client/src/pages/Notes.jsx
+++ client/src/pages/Notes.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Notes() {
   return (
-    <div>
+    <div className="container">
       <h2>Your Notes</h2>
       <p>Notes will be fetched and displayed here using GraphQL.</p>
     </div>
